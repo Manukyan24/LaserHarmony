550685dd5c9d8354cfff6c6b41de81292780c661
CarrotKishibe <Kostanyan_2005@mail.ru>
2023-08-08 12:24:06 +0400

    Adding 'Arduino signal simulator'

---
+++ UI/ArduinoEmulation.py
@@ -0,0 +1,30 @@
+import random
+import time
+#cash = {"num": None}
+def signal_emulator():
+    print(random.randint(0, 127))
+    time.sleep(2)
+    signal_emulator()
+signal_emulator()
+
+
+
+
+
+#this may be useful, but we are not gonna use it(maybe)
+    #if google == 10:
+    #    return 0
+    #else:
+    #    cash["num"] = random.randint(0, 127)
+    #    print(cash)
+    #    signal_emulator(google+1)
+
+#cash = {"num": None}
+#def signal_emulator(google = 0):
+#    cash["num"] = random.randint(0, 127)
+#    print(cash)
+#    signal_emulator(google+1)
+#    if google == 10:
+#        return 0
+#
+#signal_emulator()
